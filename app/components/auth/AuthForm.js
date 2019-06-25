15bc7bb214a08b1b57cc8781437b49249ff13059
Nas <nassersanou23@gmail.com>
2019-06-24 20:15:49 -0400

    updates

--- app/components/auth/AuthForm.js
+++ app/components/auth/AuthForm.js
@@ -1,13 +1,21 @@
 import React, { Component } from 'react';
-import { StyleSheet, View, Text } from 'react-native';
+import {
+  StyleSheet,
+  View,
+  Text,
+  Button,
+  TouchableOpacity,
+  Platform
+} from 'react-native';
 import Input from '../../utils/forms/Input';
 
 class AuthForm extends Component {
   state = {
     type: 'Login',
     action: 'Login',
-    actionMode: 'I want to regidter',
-    hasErrors: true,
+    actionMode: 'I want to register',
+    hasErrors: false,
+
     form: {
       email: {
         value: '',
@@ -18,6 +26,7 @@ class AuthForm extends Component {
           isEmail: true
         }
       },
+
       password: {
         value: '',
         valid: false,
@@ -27,12 +36,13 @@ class AuthForm extends Component {
           minLength: 6
         }
       },
+
       confirmPassword: {
         value: '',
         valid: false,
         type: 'textinput',
         rules: {
-          confirmPass: 'confoirmPassword'
+          confirmPass: 'confirmPassword'
         }
       }
     }
@@ -40,7 +50,7 @@ class AuthForm extends Component {
 
   updateInput = (name, value) => {
     this.setState({
-      hasErrors: false
+      hasErrors: true
     });
     let formCopy = this.state.form;
     formCopy[name].value = value;
@@ -51,31 +61,45 @@ class AuthForm extends Component {
   };
 
   formHasErrors = () => {
-    this.state.hasErrors ? (
-      <View style={styles.errorContainer}>
-        <Text style={styles.errorLabel}>Ooops somethings went wrong</Text>
-      </View>
-    ) : null;
+    // alert('hello ');
+    if (this.state.hasErrors) {
+      return (
+        <View style={styles.errorContainer}>
+          <Text style={styles.errorLabel}>Ooops somethings went wrong</Text>
+        </View>
+      );
+    }
   };
 
   confirmPassword = () => {
-    this.state.type != 'Login' ? (
-      <Input
-        placeholder="Confirm password"
-        placeholderTextColor="black"
-        type={this.state.form.confirmPassword.type}
-        value={this.state.form.confirmPassword.value}
-        onChangeText={value => this.updateInput('confirmPassword', value)}
-        secureTextEntry
-      />
-    ) : null;
+    if (this.state.type != 'Login') {
+      return (
+        <Input
+          placeholder="Confirm password"
+          placeholderTextColor="black"
+          type={this.state.form.confirmPassword.type}
+          value={this.state.form.confirmPassword.value}
+          onChangeText={value => this.updateInput('confirmPassword', value)}
+          secureTextEntry
+        />
+      );
+    } else {
+      return null;
+    }
   };
-
+  changeFormType = () => {
+    const type = this.state.type;
+    this.setState({
+      type: type === 'Login' ? 'Register' : 'Login',
+      action: type === 'Login' ? 'Register' : 'Login',
+      actionMode: type === 'Login' ? 'I want to Login' : 'I want to register'
+    });
+  };
   render() {
     return (
       <View>
         <Input
-          placeholder="Enter email"
+          placeholder="Enter Email"
           placeholderTextColor="black"
           autoCapitalize={'none'}
           type={this.state.form.email.type}
@@ -94,22 +118,128 @@ class AuthForm extends Component {
 
         {this.confirmPassword()}
         {this.formHasErrors()}
+
+        <View>
+          <View style={{ marginTop: 7 }}>
+            <TouchableOpacity
+              style={styles.submitBtnAttr}
+              onPress={() => navigate('HomeScreen')}
+              underlayColor="#fff"
+            >
+              <Text style={styles.loginText}>{this.state.action}</Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+
+        {/* <View style={{ marginTop: 15 }}>
+          <View style={styles.regBlueBtn}>
+            <TouchableOpacity
+              title={this.state.action}
+              onPress={this.submitUser}
+              // color={'#2196F3'}
+            >
+              <Text style={styles.loginText}>Register</Text>
+            </TouchableOpacity>
+          </View>
+        </View> */}
+
+        <View style={{ marginTop: 15 }}>
+          <View style={styles.regBlueBtn}>
+            <TouchableOpacity
+              // title={this.state.actionMode}
+              onPress={this.changeFormType}
+              // color={'#1A73E8'}
+            >
+              <Text style={styles.loginText}>{this.state.actionMode}</Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+        <View style={{ marginTop: 15 }}>
+          <View style={styles.regBlueBtn}>
+            <TouchableOpacity
+              // title={this.state.actionMode}
+              onPress={() => this.props.goNext()}
+              // color={'#1A73E8'}
+            >
+              <Text style={styles.loginText}>I'll do it later</Text>
+            </TouchableOpacity>
+          </View>
+        </View>
       </View>
     );
   }
 }
-
 const styles = StyleSheet.create({
   errorContainer: {
-    backgroundColor: '#f44336',
+    flex: 1,
+    backgroundColor: 'red',
     marginBottom: 10,
-    marginTop: 30
+    marginTop: 30,
+    padding: 10
   },
   errorLabel: {
     color: '#FFF',
     textAlignVertical: 'center',
     textAlign: 'center'
+  },
+  submitBtnAttr: {
+    ...Platform.select({
+      ios: {
+        marginRight: 10,
+        marginLeft: 10,
+        backgroundColor: '#1A73E8',
+        marginBottom: 10,
+        marginTop: 0,
+        paddingTop: 7,
+        paddingBottom: 7,
+        borderColor: '#1A73E8',
+        borderRadius: 3,
+        borderWidth: 3
+      },
+      android: {
+        marginRight: 40,
+        marginLeft: 40,
+        marginBottom: 10,
+        marginTop: 0,
+        paddingTop: 7,
+        paddingBottom: 7,
+        backgroundColor: '#1A73E8',
+        borderRadius: 3,
+        borderWidth: 3,
+        borderColor: '#1A73E8'
+      }
+    })
+    // marginRight: 40,
+    // marginLeft: 40,
+    // marginBottom: 10,
+    // marginTop: 0,
+    // paddingTop: 7,
+    // paddingBottom: 7,
+    // backgroundColor: '#1A73E8',
+    // borderRadius: 3,
+    // borderWidth: 3,
+    // borderColor: '#1A73E8'
+    // backgroundColor: '#2196F3'
+  },
+  loginText: {
+    color: '#fff',
+    textAlign: 'center',
+    paddingLeft: 10,
+    paddingRight: 10,
+    fontSize: 18
+  },
+  regBlueBtn: {
+    marginRight: 40,
+    marginLeft: 40,
+    // marginBottom: 20,
+    marginTop: 0,
+    paddingTop: 5,
+    paddingBottom: 5,
+    borderRadius: 3,
+    borderWidth: 3,
+    borderColor: '#2196F3',
+    backgroundColor: '#2196F3',
+    fontSize: 20
   }
 });
-
 export default AuthForm;
